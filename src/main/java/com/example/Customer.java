b5a1ac273c69ef5278cd93ff23e7cb058f01d21a
premprakashrohan <premprakashrohan@gmail.com>
2020-02-12 08:49:47 +0530

    Using properties, changing port,and setting some constraint. Create sample API example

---
+++ src/main/java/com/example/Customer.java
@@ -0,0 +1,118 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Customer {
+	private int id;
+	private String name;
+	private Date dob;
+
+	public Customer() {
+		 super();
+	}
+
+	public Customer(int id, String name, Date dob) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dob = dob;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+}
+
+@Component
+class CustomerRepo {
+	Map<Integer, Customer> customerMap = Stream
+			.of(new Object[][] { { 1, new Customer(1, "prem", new Date(1980, 02, 14)) },
+					{ 2, new Customer(2, "krish", new Date(2014, 04, 12)) }, })
+			.collect(Collectors.toMap(data -> (Integer) data[0], data -> (Customer) data[1]));
+
+	public void add(Customer customer) {
+		if (customer.getId() == 0) {
+			customer.setId(customerMap.size() + 1);
+
+		}
+		customerMap.put(customer.getId(), customer);
+	}
+
+	public Customer get(int id) {
+		return customerMap.get(id);
+	}
+
+	public List<Customer> getAll() {
+		List<Customer> key = new ArrayList<>();
+
+		customerMap.entrySet().stream().forEach(entry -> {
+			key.add(entry.getValue());
+		});
+
+		return key;
+	}
+	
+	public void delete(int id) {
+		customerMap.remove(id);
+	}
+
+}
+
+@Service
+class CustomerService {
+	@Autowired
+	CustomerRepo repo;
+
+	public void add(Customer customer) {
+		repo.add(customer);
+	}
+
+	public Customer get(int id) {
+		return repo.get(id);
+	}
+
+	public List<Customer> getAll() {
+		return repo.getAll();
+	}
+	
+	public void delete(int id) {
+		repo.delete(id);
+	}
+
+}
+
--- src/main/java/com/example/SprinbootAllApplication.java
+++ src/main/java/com/example/SprinbootAllApplication.java
@@ -1,8 +1,15 @@
 package com.example;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +41,35 @@ class HelloWorldController {
 	public HelloWorldBean helloWorldByBean(@RequestParam("name") String name) {
 		return new HelloWorldBean("Hello " + name + "!!!");
 	}
+	@GetMapping(value = "/hwobpv/{name}", produces = "application/json")// myApp/hwob/prem
+	public HelloWorldBean helloWorldByBeanByPathVariable(@PathVariable("name") String name) {
+		return new HelloWorldBean("Hello " + name + "!!!");
+	}
 
+}
+@RestController
+@RequestMapping(value = "/customer")
+class CutomerController {
+	@Autowired
+	CustomerService customerService;
+
+	@GetMapping(value = "/", produces = "application/json")
+	public List<Customer> helloWorldByBean() {
+		return customerService.getAll();
+	}
+
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public Customer helloWorldByBeanByPathVariable(@PathVariable("id") int id) {
+		return customerService.get(id);
+	}
+	@PostMapping(value = "/", consumes = "application/json")
+	public void save(@RequestBody Customer customer) {
+		 customerService.add(customer);
+	}
+	
+
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable Integer id) {
+		customerService.delete(id);
+	}
 }
