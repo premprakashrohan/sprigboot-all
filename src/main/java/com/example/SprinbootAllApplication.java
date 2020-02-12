a27fad5dd77698927da2bd08078a594480ed28dc
premprakashrohan <premprakashrohan@gmail.com>
2020-02-12 12:15:17 +0530

    update

--- src/main/java/com/example/SprinbootAllApplication.java
+++ src/main/java/com/example/SprinbootAllApplication.java
@@ -41,35 +41,36 @@ class HelloWorldController {
 	public HelloWorldBean helloWorldByBean(@RequestParam("name") String name) {
 		return new HelloWorldBean("Hello " + name + "!!!");
 	}
-	@GetMapping(value = "/hwobpv/{name}", produces = "application/json")// myApp/hwob/prem
+
+	@GetMapping(value = "/hwobpv/{name}", produces = "application/json") // myApp/hwob/prem
 	public HelloWorldBean helloWorldByBeanByPathVariable(@PathVariable("name") String name) {
 		return new HelloWorldBean("Hello " + name + "!!!");
 	}
 
 }
+
 @RestController
-@RequestMapping(value = "/customer")
 class CutomerController {
 	@Autowired
 	CustomerService customerService;
 
-	@GetMapping(value = "/", produces = "application/json")
-	public List<Customer> helloWorldByBean() {
+	@GetMapping(value = "/customers", produces = "application/json")
+	public List<Customer> getAllCustomer() {
 		return customerService.getAll();
 	}
 
-	@GetMapping(value = "/{id}", produces = "application/json")
-	public Customer helloWorldByBeanByPathVariable(@PathVariable("id") int id) {
+	@GetMapping(value = "/customers/{id}", produces = "application/json")
+	public Customer getCustomerById(@PathVariable("id") int id) {
 		return customerService.get(id);
 	}
-	@PostMapping(value = "/", consumes = "application/json")
-	public void save(@RequestBody Customer customer) {
-		 customerService.add(customer);
-	}
-	
 
-	@DeleteMapping(value = "/{id}")
-	public void delete(@PathVariable Integer id) {
+	@PostMapping(value = "/customers", consumes = "application/json")
+	public void saveCustomer(@RequestBody Customer customer) {
+		customerService.add(customer);
+	}
+
+	@DeleteMapping(value = "/customers/{id}")
+	public void deleteCustomer(@PathVariable Integer id) {
 		customerService.delete(id);
 	}
 }
